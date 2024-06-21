73bceb086c3b855c25d3330f727b50cd5fd8f657
amr emara <amr.emara@yallafelsekk>
2024-06-21 23:26:51 +0300

    cors

--- app.js
+++ app.js
@@ -1,39 +1,42 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { createConnection } = require('typeorm');
-const logger = require('winston');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { createConnection } = require("typeorm");
+const logger = require("winston");
 const errorMiddleware = require("./middlewares/errorMiddleware");
-const {loggerMiddleware} = require("./middlewares/logger");
+const { loggerMiddleware } = require("./middlewares/logger");
 const app = express();
 const port = 8000;
+const cors = require("cors");
 
 logger.add(new logger.transports.Console());
-
+app.use(cors);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(loggerMiddleware);
 app.use(errorMiddleware);
 
 // Database connection
-createConnection().then(async connection => {
-    console.log('Connected to the database');
+createConnection()
+  .then(async (connection) => {
+    console.log("Connected to the database");
 
     // Routes
-    const userRoutes = require('./routes/userRoutes');
-    const paperRoutes = require('./routes/paperRoutes');
+    const userRoutes = require("./routes/userRoutes");
+    const paperRoutes = require("./routes/paperRoutes");
 
-    app.use('/users', userRoutes);
-    app.use('/papers', paperRoutes);
-    app.use('/', (req, res) => {
-        res.send("`Server is running!")
-    })    
-    app.listen(port, () => {
-        console.log(`Server is running on http://localhost:${port}`);
+    app.use("/users", userRoutes);
+    app.use("/papers", paperRoutes);
+    app.use("/", (req, res) => {
+      res.send("`Server is running!");
     });
-}).catch(error => console.log(error));
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  })
+  .catch((error) => console.log(error));
 
-function s (err, req, res, next){
-    // Extract the error message from the error object
-    const errorMessage = err.message || 'Internal Server Error';
-    res.status(500).json({ message: errorMessage });
+function s(err, req, res, next) {
+  // Extract the error message from the error object
+  const errorMessage = err.message || "Internal Server Error";
+  res.status(500).json({ message: errorMessage });
 }
