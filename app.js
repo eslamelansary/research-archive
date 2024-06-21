7c70f77bdcaaf02cbbc0399ca73e61a92ce845fd
Eslam El-Ansary <eslam.elansary@yallafelsekka.com>
2024-06-21 23:44:48 +0300

    ur pad

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const port = 8000;
 const cors = require("cors");
 
 logger.add(new logger.transports.Console());
-app.use(cors);
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(loggerMiddleware);
